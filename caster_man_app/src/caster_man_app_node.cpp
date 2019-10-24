3e095552b766d09a2296ae4441676b5a66bcaead
Ye Tian <ye.tian@iquotient-robotics.com>
2019-10-25 02:05:51 +0800

    fix xacro

--- caster_man_app/src/caster_man_app_node.cpp
+++ caster_man_app/src/caster_man_app_node.cpp
@@ -32,89 +32,6 @@ void SetArmPose(moveit::planning_interface::MoveGroupInterface& move_group, std:
   move_group.move();
 }
 
-void openGripper(trajectory_msgs::JointTrajectory& posture) {
-  posture.joint_names.resize(3);
-  posture.joint_names[0] = "j2s7s300_joint_finger_1";
-  posture.joint_names[1] = "j2s7s300_joint_finger_2";
-  posture.joint_names[2] = "j2s7s300_joint_finger_3";
-
-  posture.points.resize(1);
-  posture.points[0].positions.resize(3);
-  posture.points[0].positions[0] = 0.1;
-  posture.points[0].positions[1] = 0.1;
-  posture.points[0].positions[2] = 0.1;
-  posture.points[0].time_from_start = ros::Duration(0.5);
-}
-
-void closedGripper(trajectory_msgs::JointTrajectory& posture) {
-  posture.joint_names.resize(3);
-  posture.joint_names[0] = "j2s7s300_joint_finger_1";
-  posture.joint_names[1] = "j2s7s300_joint_finger_2";
-  posture.joint_names[2] = "j2s7s300_joint_finger_3";
-
-  posture.points.resize(1);
-  posture.points[0].positions.resize(3);
-  posture.points[0].positions[0] = 1.1;
-  posture.points[0].positions[1] = 1.1;
-  posture.points[0].positions[2] = 1.1;
-  posture.points[0].time_from_start = ros::Duration(0.5);
-}
-
-// void pick(moveit::planning_interface::MoveGroupInterface& move_group) {
-//   grasps[0].grasp_pose.header.frame_id = "object";
-
-//   tf2::Quaternion orientation;
-//   orientation.setRPY(0, 0, 0);
-//   grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-//   grasps[0].grasp_pose.pose.position.x = 0.0;
-//   grasps[0].grasp_pose.pose.position.y = 0.03;
-//   grasps[0].grasp_pose.pose.position.z = -0.02;
-
-//   grasps[0].pre_grasp_approach.direction.header.frame_id = "j2s7s300_end_effector";
-//   grasps[0].pre_grasp_approach.direction.vector.z = 1.0;
-//   grasps[0].pre_grasp_approach.min_distance = 0.16;
-//   grasps[0].pre_grasp_approach.desired_distance = 0.18;
-
-//   grasps[0].post_grasp_retreat.direction.header.frame_id = "j2s7s300_end_effector";
-//   grasps[0].post_grasp_retreat.direction.vector.z = -1.0;
-//   grasps[0].post_grasp_retreat.direction.vector.y = 0.7;
-//   grasps[0].post_grasp_retreat.min_distance = 0.12;
-//   grasps[0].post_grasp_retreat.desired_distance = 0.15;
-
-//   openGripper(grasps[0].pre_grasp_posture);
-//   closedGripper(grasps[0].grasp_posture);
-
-//   move_group.pick("marker_88", grasps);
-// }
-
-// void place(moveit::planning_interface::MoveGroupInterface& move_group) {
-//   std::vector<moveit_msgs::PlaceLocation> place_location;
-//   place_location.resize(1);
-
-//   place_location[0].place_pose.header.frame_id = "j2s7s300_link_base";
-//   tf2::Quaternion orientation;
-//   orientation.setRPY(M_PI/2.0, 0.00, -M_PI/2.0);
-//   place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
-//   place_location[0].place_pose.pose.position.x = -0.519f;
-//   place_location[0].place_pose.pose.position.y = 0.336f;
-//   place_location[0].place_pose.pose.position.z = 0.515f;
-
-//   place_location[0].pre_place_approach.direction.header.frame_id = "j2s7s300_end_effector";
-//   place_location[0].pre_place_approach.direction.vector.y = -0.6f;
-//   place_location[0].pre_place_approach.direction.vector.z = 1.0f;
-//   place_location[0].pre_place_approach.min_distance = 0.08f;
-//   place_location[0].pre_place_approach.desired_distance = 0.10f;
-
-//   place_location[0].post_place_retreat.direction.header.frame_id = "j2s7s300_end_effector";
-//   place_location[0].post_place_retreat.direction.vector.z = -1.0f;
-//   place_location[0].post_place_retreat.min_distance = 0.1f;
-//   place_location[0].post_place_retreat.desired_distance = 0.25f;
-
-//   openGripper(place_location[0].post_place_posture);
-
-//   move_group.place("marker_88", place_location);
-// }
-
 void MovebaseFeedbackCallback(const move_base_msgs::MoveBaseFeedbackConstPtr& feedback) {
   ROS_INFO_STREAM("Move base feedback callback");
 }
@@ -259,11 +176,9 @@ int main(int argc, char** argv) {
   ROS_INFO("Set Height 0.16");
   SetHeight(body_pub, 0.16);
 
-  // move to standby goal
   ROS_INFO("Moveto standby goal");
   // MoveToGoal(move_base_client, standby_pose);
 
-  // set arm to standby pose
   ROS_INFO("Set arm to standby pose");
   SetArmPose(arm_group, arm_standby_pose);
 
@@ -278,11 +193,9 @@ int main(int argc, char** argv) {
   ROS_INFO("Start service...");
   ros::ServiceServer pickup_service = nh.advertiseService("ros_sharp/PickGift", PickGiftCB);
 
-  // moveit::planning_interface::MoveGroupInterface::Plan plan;
   geometry_msgs::PoseStamped target;
   get_new_target = -1;
   double r_height;
-  // std::vector<double> gripper_pose(3);
   while(ros::ok()) {
     if(get_new_target < 1 || get_new_target > 6) {
       ros::WallDuration(1.0).sleep();
@@ -291,7 +204,7 @@ int main(int argc, char** argv) {
 
     ROS_INFO("Get new target %d", get_new_target);
 
-    // move to pick pose
+    ROS_INFO("Move to pick goal");
     // MoveToGoal(move_base_client, pick_pose);
 
     if(get_new_target < 3) {
@@ -302,83 +215,82 @@ int main(int argc, char** argv) {
       r_height = 0.01;
     }
     ROS_INFO("Set height %lf", r_height);
-    SetHeight(body_pub, r_height);
+    // SetHeight(body_pub, r_height);
 
     // set arm to target box watch pose
-    SetArmPose(arm_group, arm_box_pose[get_new_target]);
+    ROS_INFO("Set arm to watch pose");
+    // SetArmPose(arm_group, arm_box_pose[get_new_target]);
 
     ROS_INFO("Update object");
     // UpdateObject(planning_scene_interface);
 
     ROS_INFO("Set arm to pre-pick pose");
-    target.header.stamp = ros::Time::now();
-    target.header.frame_id = "object";
-    target.pose.position.y = 0.02;
-    target.pose.position.z = -0.10;
-    arm_group.setPoseTarget(target);
-    arm_group.move();
+    // target.header.stamp = ros::Time::now();
+    // target.header.frame_id = "object";
+    // target.pose.position.y = 0.02;
+    // target.pose.position.z = -0.10;
+    // arm_group.setPoseTarget(target);
+    // arm_group.move();
 
     ROS_INFO("Open Gripper");
-    gripper_pose[0] = 0.02;
-    gripper_pose[1] = 0.02;
-    gripper_pose[2] = 0.02;
-    gripper_group.setJointValueTarget(gripper_pose);
-    gripper_group.move();
+    // gripper_pose[0] = 0.02;
+    // gripper_pose[1] = 0.02;
+    // gripper_pose[2] = 0.02;
+    // gripper_group.setJointValueTarget(gripper_pose);
+    // gripper_group.move();
 
     ROS_INFO("Set arm to pick pose");
-    target.header.stamp = ros::Time::now();
-    target.header.frame_id = "object";
-    target.pose.position.y = 0.02;
-    target.pose.position.z = -0.03;
-    arm_group.setPoseTarget(target);
-    arm_group.move();
+    // target.header.stamp = ros::Time::now();
+    // target.header.frame_id = "object";
+    // target.pose.position.y = 0.02;
+    // target.pose.position.z = -0.03;
+    // arm_group.setPoseTarget(target);
+    // arm_group.move();
 
     ROS_INFO("Close Gripper");
-    gripper_pose[0] = 1.2;
-    gripper_pose[1] = 1.2;
-    gripper_pose[2] = 1.2;
-    gripper_group.setJointValueTarget(gripper_pose);
-    gripper_group.move();
+    // gripper_pose[0] = 1.2;
+    // gripper_pose[1] = 1.2;
+    // gripper_pose[2] = 1.2;
+    // gripper_group.setJointValueTarget(gripper_pose);
+    // gripper_group.move();
 
     ROS_INFO("Set arm to retreat-pick pose");
-    target.header.stamp = ros::Time::now();
-    target.header.frame_id = "object";
-    target.pose.position.y = 0.10;
-    target.pose.position.z = -0.20;
-    arm_group.setPoseTarget(target);
-    arm_group.move();
+    // target.header.stamp = ros::Time::now();
+    // target.header.frame_id = "object";
+    // target.pose.position.y = 0.10;
+    // target.pose.position.z = -0.20;
+    // arm_group.setPoseTarget(target);
+    // arm_group.move();
 
-    ros::WallDuration(1.0).sleep();
-
-    // place box on table
-    SetArmPose(arm_group, arm_standby_pose);
-
-    // move to place pose
-    // MoveToGoal(move_base_client, place_pose);
+    ROS_INFO("Hold the box")
+    // SetArmPose(arm_group, arm_standby_pose);
 
     ROS_INFO("Set Height 0.16");
-    SetHeight(body_pub, 0.16);
+    // SetHeight(body_pub, 0.16);
+
+    ROS_INFO("Move to table")
+    // MoveToGoal(move_base_client, place_pose);
 
     ROS_INFO("Set arm to pre-place pose");
-    SetArmPose(arm_group, arm_standby_pose);
+    // SetArmPose(arm_group, arm_standby_pose);
+
+    ROS_INFO("Set arm to pre-place pose");
+    // SetArmPose(arm_group, arm_standby_pose);
 
     ROS_INFO("Open Gripper");
-    gripper_pose[0] = 0.02;
-    gripper_pose[1] = 0.02;
-    gripper_pose[2] = 0.02;
-    gripper_group.setJointValueTarget(gripper_pose);
-    gripper_group.move();
+    // gripper_pose[0] = 0.02;
+    // gripper_pose[1] = 0.02;
+    // gripper_pose[2] = 0.02;
+    // gripper_group.setJointValueTarget(gripper_pose);
+    // gripper_group.move();
 
-    // set arm to home pose
     ROS_INFO("Set arm to home pose");
-    SetArmPose(arm_group, arm_standby_pose);
+    // SetArmPose(arm_group, arm_standby_pose);
 
-    ros::WallDuration(1.0).sleep();
-
-    // back to standby pose
+    ROS_INFO("Back to standby pose");
     // MoveToGoal(move_base_client, standby_pose);
 
-    // reset flag
+    ROS_INFO("Reset flag");
     get_new_target = -1;
 
     ROS_INFO("All finish");
